116175954af8677ec9ad55392ea045124816de45
Matt Gallucci <mattgallucci97@gmail.com>
2022-11-10 19:54:38 -0800

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 let count = 0;
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(cors());
 
 app.get('/count', (req, res, next) => {
   res.status(200).json(count);
