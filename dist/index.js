073b7ca7c16a89c15a6dcd0901a76c9fcfaef002
Matt Gallucci <mattgallucci97@gmail.com>
2022-11-13 19:59:12 -0800

    containerized

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 let count = 0;
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 8080;
+app.use((0, cors_1.default)());
 app.get('/count', (req, res, next) => {
     res.status(200).json(count);
 });
